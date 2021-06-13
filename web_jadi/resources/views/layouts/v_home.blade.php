d8f670caeb5e3673bdf53612af955690dd261e2f
RifqiRE17 <72175584+RifqiRE17@users.noreply.github.com>
2021-06-13 20:39:02 +0700

    pembaruhan

---
+++ web_jadi/resources/views/layouts/v_home.blade.php
@@ -0,0 +1,245 @@
+@extends('layouts.master')
+@section('content')
+
+<section class="slider_section">
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+
+                <div class="container">
+                    <div class="carousel-caption">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="text-bg">
+                                    <span>Keep Healthy</span>
+                                    <h1>Dengan Hidup Sehat Segala Penyakit Bisa Dicegah</h1>
+                                    <p>Dengan aplikasi kami anda daoat menyewa jasa pembersihan rumah dengan mudah dan
+                                        cepat hanya dengan melakukan beberapa klik pada smartphone anda. Maka kami akan
+                                        siap datang kerumah anda dengan segera</p>
+                                    <a href="#"><img style=" width: 120px; height: 120px;" src="icon/playstore.png"
+                                            alt="#" />Dapatkan DI Google Play</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+
+                <div class="container ">
+                    <div class="carousel-caption">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="text-bg">
+                                    <span>Keep Healthy</span>
+                                    <h1>Dengan Hidup Sehat Segala Penyakit Bisa Dicegah</h1>
+                                    <p>Dengan aplikasi kami anda daoat menyewa jasa pembersihan rumah dengan mudah dan
+                                        cepat hanya dengan melakukan beberapa klik pada smartphone anda. Maka kami akan
+                                        siap datang kerumah anda dengan segera</p>
+                                    <a href="#"><img style=" width: 120px; height: 120px;" src="icon/playstore.png"
+                                            alt="#" />Dapatkan DI Google Play</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="carousel-item">
+
+                <div class="container">
+                    <div class="carousel-caption ">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="text-bg">
+                                    <span>Keep Healthy</span>
+                                    <h1>Dengan Hidup Sehat Segala Penyakit Bisa Dicegah</h1>
+                                    <p>Dengan aplikasi kami anda daoat menyewa jasa pembersihan rumah dengan mudah dan
+                                        cepat hanya dengan melakukan beberapa klik pada smartphone anda. Maka kami akan
+                                        siap datang kerumah anda dengan segera</p>
+                                    <a href="#"><img style=" width: 120px; height: 120px;" src="icon/playstore.png"
+                                            alt="#" />Dapatkan DI Google Play</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+</section>
+</div>
+</header>
+
+
+
+<!-- about  -->
+<div id="about" class="about">
+    <div class="container">
+        <div class="row display_boxflex">
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                <div class="about-box">
+                    <h2>Membersihkan Halaman Depan</h2>
+                    <p>Salah satu layanan kami adalah dapat membersihkan halaman depan anda dan anda dapat memilih
+                        layanan ini saat anda membutuhkannya</p>
+                    <!-- <a href="Javascript:void(0)">Read More</a> -->
+                </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                <div class="about-box">
+                    <figure><img src="images/about.png" alt="#" /></figure>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- end abouts -->
+
+
+
+
+<!-- upcoming -->
+<div id="upcoming" class="upcoming">
+    <div class="container-fluid padding_left3">
+        <div class="row display_boxflex">
+            <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                <div class="box_text">
+                    <div class="titlepage">
+                        <h2><br>Bersihkan Kamar Mandi</h2>
+                    </div>
+                    <p>Kami juga memiliki layanan untuk membersihkan kamar mandi di rumah anda sehingga anda juga dapat
+                        memilih layanan ini saat anda membutuhkannya</p>
+                    <!-- <a href="Javascript:void(0)">Read More</a> -->
+                </div>
+            </div>
+
+            <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 border_right">
+                <div class="upcomimg">
+                    <figure><img src="images/up.jpg" alt="#" /></figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end upcoming -->
+
+<!-- upcoming2 -->
+<div id="upcoming" class="upcoming">
+    <div class="container-fluid padding_left3">
+        <div class="row display_boxflex">
+            <div class="col-xl-5 col-lg-5 col-md-5 col-sm-12">
+                <div class="box_text">
+                    <div class="titlepage">
+                        <h2><br>Bersihkan Ruangan Rumah</h2>
+                    </div>
+                    <p>Dan juga terdapat layanan untuk membersihkan ruangan yang terdapat pada rumah anda sehingga anda juga dapat memilih layanan ini saat anda membutuhkannya</p>
+                    <!-- <a href="Javascript:void(0)">Read More</a> -->
+                </div>
+            </div>
+
+            <div class="col-xl-7 col-lg-7 col-md-7 col-sm-12 border_right">
+                <div class="upcomimg">
+                    <figure><img src="images/up.jpg" alt="#" /></figure>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end upcoming2 -->
+
+
+
+<!-- Gallery -->
+
+
+<div id="gallery" class="Gallery">
+    <div class="container">
+        <div class="row display_boxflex">
+            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 margi_bott">
+                        <div class="Gallery_img">
+                            <figure><img src="images/Gallery1.jpg" alt="#" /></figure>
+                        </div>
+                        <div class="hover_box">
+
+                            <ul class="icon_hu">
+                                <h3>Music night</h3>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 margi_bott">
+                        <div class="Gallery_img">
+                            <figure><img src="images/Gallery2.jpg" alt="#" /></figure>
+                        </div>
+                        <div class="hover_box">
+
+                            <ul class="icon_hu">
+                                <h3>Music night</h3>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 margi_bott1">
+                        <div class="Gallery_img">
+                            <figure><img src="images/Gallery3.jpg" alt="#" /></figure>
+                        </div>
+                        <div class="hover_box">
+
+                            <ul class="icon_hu">
+                                <h3>Music night</h3>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
+                        <div class="Gallery_img">
+                            <figure><img src="images/Gallery4.jpg" alt="#" /></figure>
+                        </div>
+                        <div class="hover_box">
+
+                            <ul class="icon_hu">
+                                <h3>Music night</h3>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                                <li><a href="#"><img src="icon/h.png" alt="#" /></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                <div class="Gallery_text">
+                    <div class="titlepage">
+                        <h2>Gallery</h2>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page
+                        when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
+                        distribution of letters, as opposed to usin</p>
+                    <!-- <a href="Javascript:void(0)">Read More</a> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- end Gallery -->
+@stop
---
+++ web_jadi/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Mitra\MitraController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+
+Route::get('/dashboard', [DashboardController::class,'dashboard']);
+
+
+
+// route untuk mitra
+// ========================================================================
+Route::get('/mitra/register', [MitraController::class, 'formAddMitra']);
+Route::post('/mitra/register', [MitraController::class, 'registerMitra']);
+Route::get('/mitra/delete/{id}', [MitraController::class, 'deleteMitra']);
+Route::get('/dashboard/mitra', [MitraController::class, 'index']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
