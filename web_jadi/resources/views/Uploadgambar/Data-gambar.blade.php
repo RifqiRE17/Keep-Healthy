20857e78c42701f77e3756a722531af4556161bb
FigoFajarYulianto <figofajar07@gmail.com>
2021-06-15 11:34:10 +0700

    perbaikan

--- web_jadi/resources/views/Uploadgambar/Data-gambar.blade.php
+++ web_jadi/resources/views/Uploadgambar/Data-gambar.blade.php
@@ -1,101 +1,107 @@
-
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
+
 <head>
-  @include('Template.head')
+    @include('Template.head')
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-  <!-- Navbar -->
-  @include('Template.navbar')
-  <!-- /.navbar -->
+        <!-- Navbar -->
+        @include('Template.navbar')
+        <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  @include('Template.sidebar')
+        <!-- Main Sidebar Container -->
+        @include('Template.sidebar')
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">ADMIN</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Data Admin</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-        <div class="card card-info card-outline">
-            <div class="card-header">
-                <div class="card-tools">
-                    <a href="{{ route('create-gambar') }}" class="btn btn-success">Tambah Data <i class="fas fa-plus-square"></i></a>
-                </div>
-                </div>
-
-            <div class="card-body">
-                <table class="table table-bordered">
-                    <tr>
-                        <th>Nama</th>
-                        <th>Jabatan</th>
-                        <th>Alamat</th>
-                        <th>Upload Foto</th>
-                        <th>Upload KTP</th>
-                        <th>Aksi</th>
-                    </tr>
-                    @foreach ($dataGambar as $item)
-                    <tr>
-                        <td>{{ $item->nama }}</td>
-                        <td>{{ $item->jabatan }}</td>
-                        <td>{{ $item->alamat }}</td>
-                        <td>
-                        <a href="{{ asset('img/'. $item->upload_foto ) }}" target="_blank" rel="noopener noreferrer">Lihat Foto</a>
-                        </td>
-                        <td>
-                        <a href="{{ asset('img/'. $item->upload_ktp ) }}" target="_blank" rel="noopener noreferrer">Lihat Foto KTP</a>
-                        </td>
-
-                        <td>
-                        <a href="{{ url('edit-gambar',$item->id) }}"><i class="fas fa-edit"></i></a> 
-                        | 
-                        <a href="{{ url('delete-gambar',$item->id) }}"><i class="fas fa-trash-alt" style="color: red"></i></a>
-                        </td>
-         
-                        
-
-                    </tr>
-                    @endforeach
-                </table>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">PEGAWAI</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Data Pegawai</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
             </div>
-            
-        </div>
-    </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <!-- Main Footer -->
-  <footer class="main-footer">
-  @include('Template.footer')
-  </footer>
-</div>
-<!-- ./wrapper -->
+            <!-- /.content-header -->
 
-<!-- REQUIRED SCRIPTS -->
-@include('Template.script')
+            <!-- Main content -->
+            <div class="content">
+                <div class="card card-info card-outline">
+                    <div class="card-header">
+                        <div class="card-tools">
+                            <a href="{{ route('create-gambar') }}" class="btn btn-success">Tambah Data <i
+                                    class="fas fa-plus-square"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <tr>
+                                <th>Nama</th>
+                                <th>Layanan Service</th>
+                                <th>Alamat</th>
+                                <th>Upload Foto</th>
+                                <th>Upload KTP</th>
+                                <th>Aksi</th>
+                            </tr>
+                            @foreach ($dataGambar as $item)
+                            <tr>
+                                <td>{{ $item->nama }}</td>
+                                <td>{{ $item->jabatan }}</td>
+                                <td>{{ $item->alamat }}</td>
+                                <td>
+                                    <a href="{{ asset('img/'. $item->upload_foto ) }}" target="_blank"
+                                        rel="noopener noreferrer">Lihat Foto</a>
+                                </td>
+                                <td>
+                                    <a href="{{ asset('img/'. $item->upload_ktp ) }}" target="_blank"
+                                        rel="noopener noreferrer">Lihat Foto KTP</a>
+                                </td>
+
+                                <td>
+                                    <a href="{{ url('edit-gambar',$item->id) }}"><i class="fas fa-edit"></i></a>
+                                    |
+                                    <a href="{{ url('delete-gambar',$item->id) }}"><i class="fas fa-trash-alt"
+                                            style="color: red"></i></a>
+                                </td>
+
+
+
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            @include('Template.footer')
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+    @include('Template.script')
 
 </body>
+
 </html>
