006084274206bba08e14147641e7c05aa3d5c9fb
FigoFajarYulianto <figofajar07@gmail.com>
2021-06-15 11:14:08 +0700

    perbaikan

--- web_jadi/resources/views/Uploadartikel/Create-artikel.blade.php
+++ web_jadi/resources/views/Uploadartikel/Create-artikel.blade.php
@@ -1,96 +1,113 @@
-
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
+
 <head>
-  @include('Template.head')
+    @include('Template.head')
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-  <!-- Navbar -->
-  @include('Template.navbar')
-  <!-- /.navbar -->
+        <!-- Navbar -->
+        @include('Template.navbar')
+        <!-- /.navbar -->
 
-  <!-- Main Sidebar Container -->
-  @include('Template.sidebar')
+        <!-- Main Sidebar Container -->
+        @include('Template.sidebar')
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Data Artikel</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-        <div class="card card-info card-outline">
-            <div class="card-header">
-            <h3>Create Data Artikel</h3>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Starter Page</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Data Artikel</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
             </div>
+            <!-- /.content-header -->
 
-            <div class="card-body">
-                <form action="{{ route('simpan-artikel') }}" method="post" enctype="multipart/form-data">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label class="control-label" for="namaartikel">Nama Artikel</label>
-                    <input type="text" id="namaartikel" name="namaartikel" placeholder="Nama Artikel" class="form-control @error('namaartikel')
+            <!-- Main content -->
+            <div class="content">
+                <div class="card card-info card-outline">
+                    <div class="card-header">
+                        <h3>Create Data Artikel</h3>
+                    </div>
+
+                    <div class="card-body">
+                        <form action="{{ route('simpan-artikel') }}" method="post" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label class="control-label" for="namaartikel">Nama Artikel</label>
+                                <input type="text" id="namaartikel" name="namaartikel" placeholder="Nama Artikel" class="form-control @error('namaartikel')
                     is-invalid @enderror" autofocus>
-                    @error('namaartikel')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                  </div>
-                  <div class="form-group">
-                  <label class="control-label" for="isiartikel">Isi Artikel</label>
-                    <input type="text" id="isiartikel" name="isiartikel" placeholder="Isi Artikel" class="form-control @error('isiartikel')
-                    is-invalid @enderror" autofocus>
-                    @error('isiartikel')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                    @enderror
-                  </div>
-                  <div class="form-group">
-                  <label class="control-label" for="upload_foto">Foto Artikel</label>
-                    <input type="file" id="upload_foto" name="upload_foto" class="form-control @error('upload_foto')
+                                @error('namaartikel')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label>Isi Artikel</label>
+                                <textarea id="summernote" name="isiartikel" placeholder="Isi Artikel"
+                                    class="textarea"></textarea>
+                                @error('conten')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label" for="upload_foto">Foto Artikel</label>
+                                <input type="file" id="upload_foto" name="upload_foto" class="form-control @error('upload_foto')
                     is-invalid @enderror" autofoucus>
-                    @error('upload_foto')
-                    <div class="invalid-feedback">{{ $message}}</div>
-                    @enderror
-                  </div>
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-success">Simpan Data</button>
-                  </div>
-                </form>
+                                @error('upload_foto')
+                                <div class="invalid-feedback">{{ $message}}</div>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-success">Simpan Data</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
+            <!-- /.content -->
         </div>
+        <!-- /.content-wrapper -->
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            @include('Template.footer')
+        </footer>
     </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-  <!-- Main Footer -->
-  <footer class="main-footer">
-  @include('Template.footer')
-  </footer>
-</div>
-<!-- ./wrapper -->
 
-<!-- REQUIRED SCRIPTS -->
-@include('Template.script')
+    <!-- ./wrapper -->
+
+    <!-- REQUIRED SCRIPTS -->
+    @include('Template.script')
+
+    @push('styles')
+    <link href="{{asset('vendor/summernote/summernote.min.css')}}" rel="stylesheet">
+    @endpush
+
+    @push('script')
+    <script type="text/javascript" src="{{asset('vendor/summernote/summernote.min.js')}}"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
+    </script>
+    @endpush
 
 </body>
+
 </html>
